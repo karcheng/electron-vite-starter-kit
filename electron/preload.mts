b7be9a24648198ded810d863d014b804eb7ac29f
karcheng <karcheng01@hotmail.com>
2025-03-06 11:53:29 -0500

    Initial code base

--- electron/preload.mts
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('api', {
-  myApiFunction: () => {
-    return ipcRenderer.invoke('some-ipc-event');
-  }
-});
--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeAPI({
-  loadExternalComponent: () => {
-    return ipcRenderer.invoke('load-external-component');
-  },
-});
